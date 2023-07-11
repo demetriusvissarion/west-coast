03f226ecbc9ae495f989b274a7521f86eb97ffbc
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-07-11 15:28:52 +0100

    changed style and added primary menu

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -8,31 +9,50 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-$bootstrap_version = get_theme_mod( 'understrap_bootstrap_version', 'bootstrap4' );
-$navbar_type       = get_theme_mod( 'understrap_navbar_type', 'collapse' );
+$bootstrap_version = get_theme_mod('understrap_bootstrap_version', 'bootstrap4');
+$navbar_type       = get_theme_mod('understrap_navbar_type', 'collapse');
+$container = get_theme_mod('understrap_container_type');
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
-<?php do_action( 'wp_body_open' ); ?>
-<div class="site" id="page">
+	<?php do_action('wp_body_open'); ?>
+	<div class="site" id="page">
 
-	<!-- ******************* The Navbar Area ******************* -->
-	<header id="wrapper-navbar">
+		<!-- ******************* The Navbar Area ******************* -->
+		<div id="wrapper-navbar">
 
-		<a class="skip-link <?php echo understrap_get_screen_reader_class( true ); ?>" href="#content">
-			<?php esc_html_e( 'Skip to content', 'understrap' ); ?>
-		</a>
+			<div class="top-header">
+				<div class="row">
+					<div class="col-lg-5">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="logo" />
+					</div>
+					<div class="col-lg-7"></div>
+				</div>
+			</div>
 
-		<?php get_template_part( 'global-templates/navbar', $navbar_type . '-' . $bootstrap_version ); ?>
+			<nav id="main-nav" class="navbar navbar-expand-lg navbar-dark bg-primary" aria-labelledby="main-nav-label">
 
-	</header><!-- #wrapper-navbar -->
+				<div class="container">
+
+					<a class="skip-link <?php echo understrap_get_screen_reader_class(true); ?>" href="#content">
+						<?php esc_html_e('Skip to content', 'understrap'); ?>
+					</a>
+
+					<?php get_template_part('global-templates/navbar', $navbar_type . '-' . $bootstrap_version); ?>
+
+				</div>
+
+			</nav>
+
+		</div><!-- #wrapper-navbar -->
