87d4a46999dea563a6793e792feadc77c6d12db4
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-07-18 11:05:49 +0100

    internal wordpress page - About, changed sidebars

--- loop-templates/content-page.php
+++ loop-templates/content-page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Partial template for content in page.php
  *
@@ -6,20 +7,20 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 ?>
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
 	<?php
-	if ( ! is_page_template( 'page-templates/no-title.php' ) ) {
-		the_title(
-			'<header class="entry-header"><h1 class="entry-title">',
-			'</h1></header><!-- .entry-header -->'
-		);
-	}
+	// if ( ! is_page_template( 'page-templates/no-title.php' ) ) {
+	// 	the_title(
+	// 		'<header class="entry-header"><h1 class="entry-title">',
+	// 		'</h1></header><!-- .entry-header -->'
+	// 	);
+	// }
 
-	echo get_the_post_thumbnail( $post->ID, 'large' );
+	echo get_the_post_thumbnail($post->ID, 'large');
 	?>
 
 	<div class="entry-content">
--- page.php
+++ page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages
  *
@@ -11,45 +12,64 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 get_header();
 
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 
 ?>
 
+<div class="container">
+	<div class="entry-header">
+		<?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+	</div>
+</div>
+
 <div class="wrapper" id="page-wrapper">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+	<div class="<?php echo esc_attr($container); ?>" id="content" tabindex="-1">
 
 		<div class="row">
 
-			<?php
-			// Do the left sidebar check and open div#primary.
-			get_template_part( 'global-templates/left-sidebar-check' );
-			?>
+			<div class="col-lg-8">
 
-			<main class="site-main" id="main">
+				<main class="site-main" id="main">
 
-				<?php
-				while ( have_posts() ) {
-					the_post();
-					get_template_part( 'loop-templates/content', 'page' );
+					<?php
+					while (have_posts()) {
+						the_post();
+						get_template_part('loop-templates/content', 'page');
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
+						// If comments are open or we have at least one comment, load up the comment template.
+						if (comments_open() || get_comments_number()) {
+							comments_template();
+						}
 					}
-				}
-				?>
+					?>
 
-			</main>
+				</main>
 
-			<?php
-			// Do the right sidebar check and close div#primary.
-			get_template_part( 'global-templates/right-sidebar-check' );
-			?>
+			</div>
+
+			<div class="col-lg-4">
+				<div class="right-sidebar-promotion">
+
+					<div class="discount-circle">
+						<div class="abs-holder">
+							<div class="save">Save</div>
+							<div class="percent">20%</div>
+						</div>
+					</div>
+
+					<div class="entry-content">
+						<div class="title">15 Day Storm Special on Skip Bins</div>
+						<div class="desc">BIN SERVICES - Perth, Fremantle, Kwinana, Rockingham, mandurah, Pinjarra, Waroona, Harvey, Australind, Bunbury, Busselton, Margaret River</div>
+						<a href="#" class="button outline">Find out more information</a>
+					</div>
+
+				</div>
+			</div>
 
 		</div><!-- .row -->
 
